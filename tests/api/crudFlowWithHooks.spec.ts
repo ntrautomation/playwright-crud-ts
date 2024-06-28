535ec6f4a38b1f4145f8b51164eddaaa8e69dd37
Nenad Trajanovski <nenad.trajanovski@scalefocus.com>
2024-06-28 13:47:58 +0200

    test with hooks

--- tests/api/crudFlowWithHooks.spec.ts
+++ tests/api/crudFlowWithHooks.spec.ts
@@ -4,35 +4,39 @@ import {test, expect} from '@playwright/test';
 import { books } from '../../e2e/data/books.json';
 
 const bookList: string[] = [];
+let user;
+let token;
+let userInfo;
 
-test.describe('CRUD API FLOW', () => {
+test.describe('CRUD API FLOW WITH HOOKS', () => {
     let controler : Controler
    
     test.beforeEach(async ({ page, request }) => {
         controler = new Controler(page, request);
         await controler.navigate();
         Iterator.iterateOverJson(bookList, books)
-    })
+        user = await controler.createUser()
+        token = await controler.getAuthorizationToken();
+    });
 
-    test('CRUD USER FLOW', async () => {
-        const user = await controler.createUser()
-        const token = await controler.getAuthorizationToken();
-        let userInfo = await controler.getUserInformation(token.token, user.userID);
-        
-        expect(await user.userID).toEqual(await userInfo.userId)
-        expect(await user.username).toEqual(await userInfo.username);
-
+    test.afterEach(async () => {
         const deleteUser = await controler.deleteUser(token.token, user.userID);
         expect(deleteUser.status()).toEqual(204)
 
         userInfo = await controler.getUserInformation(token.token, user.userID);
         expect(userInfo.message).toEqual('User not found!')
+    })
+    
+
+    test('CRUD USER FLOW', async () => {
+        userInfo = await controler.getUserInformation(token.token, user.userID);
+        
+        expect(await user.userID).toEqual(await userInfo.userId)
+        expect(await user.username).toEqual(await userInfo.username);
     }) ;
 
     test('CRUD USER FLOW WITH ADDING AND REMOVING BOOKS', async () => {
-        const user = await controler.createUser()
-        const token = await controler.getAuthorizationToken();
-        let userInfo = await controler.getUserInformation(token.token, user.userID);
+        userInfo = await controler.getUserInformation(token.token, user.userID);
         
         expect(await user.userID).toEqual(await userInfo.userId)
         expect(await user.username).toEqual(await userInfo.username);
@@ -47,12 +51,6 @@ test.describe('CRUD API FLOW', () => {
 
         const deleteBooks = await controler.deleteUserBook(token.token, user.userID, bookList[2]);
         expect(deleteBooks.status()).toEqual(204)
-
-        const deleteUser = await controler.deleteUser(token.token, user.userID);
-        expect(deleteUser.status()).toEqual(204)
-
-        userInfo = await controler.getUserInformation(token.token, user.userID);
-        expect(userInfo.message).toEqual('User not found!')
     });
     
 })
